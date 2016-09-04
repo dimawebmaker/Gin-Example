3fd4f72582a5ab23dbe6ff34f436ed0d16835f3b
dimawebmaker <dimawebmaker@gmail.com>
2016-09-04 20:59:12 +0500

    line fix

--- common_test.go
+++ common_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-  "net/http"
-  "net/http/httptest"
-  "os"
-  "testing"
-  "github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
 var tmpUserList []user
@@ -13,48 +14,58 @@ var tmpArticleList []article
 
 // This function is used to do setup before executing the test functions
 func TestMain(m *testing.M) {
-  //Set Gin to Test Mode
-  gin.SetMode(gin.TestMode)
+	//Set Gin to Test Mode
+	gin.SetMode(gin.TestMode)
 
-  // Run the other tests
-  os.Exit(m.Run())
+	// Run the other tests
+	os.Exit(m.Run())
 }
 
 // Helper function to create a router during testing
+func getRouter(withTemplates bool) *gin.Engine {
+	r := gin.Default()
+	if withTemplates {
+		r.LoadHTMLGlob("templates/*")
+		r.Use(setUserStatus())
+	}
+	return r
+}
+
+// Helper function to process a request and test its response
 func testHTTPResponse(t *testing.T, r *gin.Engine, req *http.Request, f func(w *httptest.ResponseRecorder) bool) {
-  // Create a response recorder
-  w := httptest.NewRecord()
 
-  // Create the service and process the above request.
-  r.ServeHTTP(w, req)
+	// Create a response recorder
+	w := httptest.NewRecorder()
 
-  if !f(w) {
-    t.Fail()
-  }
+	// Create the service and process the above request.
+	r.ServeHTTP(w, req)
+
+	if !f(w) {
+		t.Fail()
+	}
 }
 
 // This is a helper function that allows us to reuse some code in the above
 // test methods
+func testMiddlewareRequest(t *testing.T, r *gin.Engine, expectedHTTPCode int) {
+	// Create a request to send to the above route
+	req, _ := http.NewRequest("GET", "/", nil)
 
-func testMiddlewareRequest(t *testing.T, r *gin.Engine, expectedHTTPcode int) {
-  // Create a request to send to the above route
-  req, _ := http.newRequest("GET", "/", nil)
-
-  // Process the request and test the response
-  testHTTPResponse(t, r, req, fun(w *httptest.ResponseRecorder) bool {
-  return w.Code == expectedHTTPcode
-})
+	// Process the request and test the response
+	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		return w.Code == expectedHTTPCode
+	})
 }
 
 // This function is used to store the main lists into the temporary one
 // for testing
-func saveList() {
-  tmpUserList = userList
-  tmpArticleList = articleList
+func saveLists() {
+	tmpUserList = userList
+	tmpArticleList = articleList
 }
 
 // This function is used to restore the main lists from the temporary one
-func restoreList() {
-  userList = tmpUserList
-  article = tmpArticleList
+func restoreLists() {
+	userList = tmpUserList
+	articleList = tmpArticleList
 }
